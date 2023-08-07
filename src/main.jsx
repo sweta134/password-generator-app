26a4fe74d6a75353a8f7aa6cc50d0a4d51cd6b72
Sweta134 <upadhyaysweta17@gmail.com>
2023-08-07 16:16:46 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+
+import App from './App';
+import './style.css';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+    <Toaster />
+  </StrictMode>
+);
